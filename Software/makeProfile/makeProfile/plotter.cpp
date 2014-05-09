171aeca68a5ed38a701ab3089be3608fad03dbac
Christopher Macklen <cmacklen@gmail.com>
2014-05-09 17:08:44 -0600

    YARC -> Changed the graph refs from indices to pointers

--- Software/makeProfile/makeProfile/plotter.cpp
+++ Software/makeProfile/makeProfile/plotter.cpp
@@ -8,28 +8,36 @@ Plotter::Plotter(MainWindow *mainwindowin, QObject *parent) :
     mainPlot = mainwindowin->get_qplot();
 }
 
+Plotter::~Plotter(){
+    delete mainPlot;
+    delete mainwindow;
+}
+
+/********************************************************************
+ * Helper Methods                                                   *
+ *******************************************************************/
+
+void Plotter::clear_all_graph_data(){
+    QMapIterator <QString, QCPGraph*> i(usedGraphs);
+    while(i.hasNext()){
+        i.next();
+        i.value()->clearData();
+    }
+}
+
+void Plotter::clear_graph_data(QString graph_name){
+    if(usedGraphs.contains(graph_name)){
+        usedGraphs.value(graph_name)->clearData();
+    }
+}
+
 void Plotter::create_graph(QString graph_name){
-    mainPlot->addGraph();
-    usedGraphs.insert(graph_name,mainPlot->graphCount()-1);
+    usedGraphs.insert(graph_name,mainPlot->addGraph());
 }
 
 void Plotter::remove_graph(QString graph_name){
     if(usedGraphs.contains(graph_name)){
         mainPlot->removeGraph(usedGraphs.value(graph_name));
         usedGraphs.remove(graph_name);
-    }
-}
-
-void Plotter::clear_graph_data(QString graph_name){
-    if(usedGraphs.contains(graph_name)){
-        mainPlot->graph(usedGraphs.value(graph_name))->clearData();
-    }
-}
-
-void Plotter::clear_all_graph_data(){
-    QMapIterator <QString, char> i(usedGraphs);
-    while(i.hasNext()){
-        i.next();
-        clear_graph_data(i.key());
     }
 }
--- Software/makeProfile/makeProfile/plotter.h
+++ Software/makeProfile/makeProfile/plotter.h
@@ -12,10 +12,50 @@ class Plotter : public QObject
     Q_OBJECT
 public:
     explicit Plotter(MainWindow *mainwindowin, QObject *parent = 0);
-    void create_graph(QString graph_name);
-    void remove_graph(QString graph_name);
-    void clear_graph_data(QString graph_name);
+    ~Plotter();
+
+    /////////////////////////////////////////////////////////////////////
+    /// \brief clear_all_graph_data
+    /// Clears all data for all graphs
+    /////////////////////////////////////////////////////////////////////
     void clear_all_graph_data();
+
+    /////////////////////////////////////////////////////////////////////
+    /// \brief clear_graph_data
+    /// \param graph_name
+    /// Clears all of the data relating to the given graph.
+    /////////////////////////////////////////////////////////////////////
+    void clear_graph_data(QString graph_name);
+
+    /////////////////////////////////////////////////////////////////////
+    /// \brief create_graph
+    /// \param graph_name
+    /// This is an abstraction of the QCustomPlot add graph, such that
+    /// the developer needs not keep track of each graph.
+    /////////////////////////////////////////////////////////////////////
+    void create_graph(QString graph_name);
+
+    /////////////////////////////////////////////////////////////////////
+    /// \brief remove_graph
+    /// \param graph_name
+    /// Remove the user indicated graph from the plot.
+    /////////////////////////////////////////////////////////////////////
+    void remove_graph(QString graph_name);
+
+    /********************************************************************
+     * Getters                                                          *
+     *******************************************************************/
+
+    /////////////////////////////////////////////////////////////////////
+    /// \brief get_used_graphs
+    /// \return
+    /// Returns the current graph-name to graph-pointer mapping
+    /////////////////////////////////////////////////////////////////////
+    const QMap<QString, QCPGraph*> &get_used_graphs() const { return usedGraphs; }
+
+    /********************************************************************
+     * Setters                                                          *
+     *******************************************************************/
 
 
 signals:
@@ -23,9 +63,20 @@ signals:
 public slots:
 
 private:
-    QMap<QString,char> usedGraphs;    // List of active graphs
+    QMap<QString,QCPGraph*> usedGraphs;    // List of active graphs
     MainWindow *mainwindow;
     QCustomPlot *mainPlot;
+
+    /********************************************************************
+     * Setters                                                          *
+     *******************************************************************/
+
+    /////////////////////////////////////////////////////////////////////
+    /// \brief set_used_graphs
+    /// \param used_graphs
+    /// Set the used graphs map, this will probably not see any usage
+    /////////////////////////////////////////////////////////////////////
+    void set_used_graphs(const QMap<QString, QCPGraph*> &used_graphs){ usedGraphs = used_graphs; }
 };
 
 #endif // PLOTTER_H
